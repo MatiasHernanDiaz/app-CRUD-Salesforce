c5250541593af22e8df059bee345b5d6bd0f9cfc
Diaz Matias Hernan <diaz.matiash@gmail.com>
2024-05-24 17:44:53 -0300

    se crea compomponente lwc clima

--- force-app/main/default/lwc/prueba/prueba.js
+++ force-app/main/default/lwc/prueba/prueba.js
@@ -4,8 +4,15 @@ import { LightningElement, wire, api } from 'lwc';
 export default class Prueba extends LightningElement {
 
     clienteA = '003aj000002Yo65AAC';
-    @wire (getDataTem, {id: "$clienteA"}) dato;
+    datosClima;
 
-    
+    @wire (getDataTem, {id: "$clienteA"})datosClima;
+    wiredCustomerData({ error, data }) {
+        if (data) {
+            this.customerData = data;
+        } else if (error) {
+            console.error('Error fetching customer data: ', error);
+        }
+    }
 
 }
