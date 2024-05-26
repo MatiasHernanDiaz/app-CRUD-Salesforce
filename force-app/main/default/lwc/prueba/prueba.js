8deec8f4c26fa4af81017b2e99667a930fc9ca36
Diaz Matias Hernan <diaz.matiash@gmail.com>
2024-05-26 20:31:59 -0300

    se obtiene regidId y se pasa a la funcion. Queda deserializar el objeto que llega de apex

--- force-app/main/default/lwc/prueba/prueba.js
+++ force-app/main/default/lwc/prueba/prueba.js
@@ -1,18 +1,34 @@
 import getDataTem from '@salesforce/apex/ControllerWeatherApi.getDataTem';
 import { LightningElement, wire, api } from 'lwc';
+import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
+import ID_FIELD from '@salesforce/schema/Contact.Id'
 
 export default class Prueba extends LightningElement {
 
-    clienteA = '003aj000002Yo65AAC';
-    datosClima;
 
-    @wire (getDataTem, {id: "$clienteA"})datosClima;
-    wiredCustomerData({ error, data }) {
+    @api recordId;
+
+
+    @wire (getDataTem, {id: "$recordId"})
+    wiredAccount({data, error}) {
         if (data) {
-            this.customerData = data;
+            this.data = data;
+            this.error = undefined;
         } else if (error) {
-            console.error('Error fetching customer data: ', error);
+            this.error = error;
+            this.data = undefined;
         }
+    }
+
+    
+    //clima = JSON.stringify( this.a );
+
+    
+
+    get temperatura(){
+        console.log(this.data);
+        console.log(this.error);
+        return  this.error;
     }
 
 }
